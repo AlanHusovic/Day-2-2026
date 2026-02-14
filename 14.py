fa48d653c0d5c2c3ba100106991ba97d6160cfcc
AlanHusovic <alanhusovic755@gmail.com>
2026-02-14 17:39:57 +0100

    Add examples of Python data types and subscripting

---
+++ 14.py
@@ -0,0 +1,15 @@
+# Subscripting
+print("Hello"[4])
+
+# Integers = whole number
+print("123" + "456")
+
+# Large integers
+print("123_456_789")
+
+# Float = Floating point number
+print("3.14538")
+
+# Boolean
+print(True)
+print(False)
