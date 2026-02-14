f7a659af59185e14c092955273f530db597243bc
AlanHusovic <alanhusovic755@gmail.com>
2026-02-14 18:19:09 +0100

    Add basic type checks and user input handling

---
+++ 15.py
@@ -0,0 +1,23 @@
+len("Hello")
+
+
+
+print(type("ČAO"))
+print(type(1234))
+print(type(1.1355))
+print(type(False))
+
+print(int("123") + int("456"))
+
+int()
+float()
+str()
+bool()
+
+name_of_the_user = input("Enter your name ")
+length_of_name = len(name_of_the_user)
+
+print(type("Enter your name"))
+print(type(length_of_name))
+
+print("Number of letters in your name:" + str(length_of_name))
